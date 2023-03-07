4a5acc34ae1eaeb05f5bb2ccc5b77ed8eee69278
santoshmondal <santosh.ece06@gmail.com>
2023-03-07 09:53:23 +0530

    handling event.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 function App() {
+  // Member Function :: ACTION HAPPEN HERE
+  let readAllGreetings = () => {
+    console.log("Handling Event...onclick event!");
+  };
+
+  // UI - RENDER
   return (
     <div>
-      <h1>Hello</h1>
-      <input type="button" value="Click Me" />
+      <h1>REST API </h1>
+      <input
+        type="button"
+        value="Read All Greetings"
+        onClick={readAllGreetings}
+      />
     </div>
   );
 }
