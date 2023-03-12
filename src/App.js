1d8be5d7cb7e185b8e0db5d2040a92dc7d697344
santoshmondal <santosh.ece06@gmail.com>
2023-03-12 11:12:34 +0530

    basics of jwt with react.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import { useState } from "react";
 function App() {
   let [list, setList] = useState([]);
 
-  let readAllGreetings = async () => {
-    let url = "http://localhost:8080/greeting/read-all-record";
-    let res = await axios.get(url);
+  let readAllOrders = async () => {
+    let url = "http://localhost:8080/order/";
+    let jwt = `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJuaWtoaWwiLCJpc3MiOiJjZGFjIiwiaWF0IjoxNjc4NTk3Mzk2LCJleHAiOjE2Nzg2MDA5OTZ9.VZ8e0xI-iPPQU0edVJQxW4kW0zjIl1FTMf8dywfKtXRIUSQlU2Z5FNCUTYP_P7A4D-cUUyQ5m2UleycQ5xty9Q`;
+
+    // let res = await axios.get(url);
+
+    let config = { headers: { Authorization: `Bearer ${jwt}` } }; // string literals
+    let res = await axios.get(url, config);
 
     // console.log(res);
     // update the list for rerender
@@ -14,24 +19,20 @@ function App() {
   };
 
   let addNewRecord = async () => {
-    let url = "http://localhost:8080/greeting/add-record";
+    let url = "http://localhost:8080/order/";
     let data = {
       message: "Helloooooooooooo",
     };
     await axios.post(url, data);
 
-    readAllGreetings();
+    readAllOrders();
   };
 
   // UI - RENDER - VIEW
   return (
     <div>
       <h1>REST API </h1>
-      <input
-        type="button"
-        value="Read All Greetings"
-        onClick={readAllGreetings}
-      />
+      <input type="button" value="Read All Order" onClick={readAllOrders} />
       <input type="button" value="Add Record" onClick={addNewRecord} />
 
       {list.map((item) => (
